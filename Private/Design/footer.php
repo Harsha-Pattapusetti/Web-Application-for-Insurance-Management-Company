e06fd80651b1be361da2f818f1da1b1e5a5b28db
Harsha Pattapusetti <88115470+Harsha-Pattapusetti@users.noreply.github.com>
2021-07-28 22:21:10 -0400

    Add files via upload

---
+++ Private/Design/footer.php
@@ -0,0 +1,7 @@
+ <footer>
+  &copy; <?php echo date('Y'); ?> WDS COMPANY
+</footer>
+
+</body>
+</html>
+
---
+++ Private/Design/header.php
@@ -0,0 +1,21 @@
+
+
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>WDS </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="<?php echo url_wds('/stylesheets/style.css'); ?>" />
+  </head>
+
+  <body>
+    <header>
+      <h1>WDS Insurance Management System</h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <center><a href="<?php echo url_wds('/data/index.php'); ?>"> BACK TO MAIN MENU </a></center>
+      </ul>
+    </navigation>
