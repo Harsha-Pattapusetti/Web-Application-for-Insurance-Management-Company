8b7ddd5eed21ea09f9dd10c42625a09f09cd23a7
Harsha Pattapusetti <88115470+Harsha-Pattapusetti@users.noreply.github.com>
2021-07-28 22:23:09 -0400

    Add files via upload

---
+++ Public/Index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>WDS Insurance Management System</title>
+    <meta charset="utf-8">
+  </head>
+<h1> WDS COMING SOON <H1>
+  <body>
+
+  </body>
+</html>
